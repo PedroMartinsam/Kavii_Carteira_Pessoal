4013f6a5fbea9179c757fa4b84dfe1d97c6daa5c
PedroMartinsam <pedromartinsstr@gmail.com>
2025-08-22 02:25:29 -0300

    first commit

---
+++ Kavii/src/main/java/com/Carteira_Pessoal/Kavii/KaviiApplication.java
@@ -0,0 +1,13 @@
+package com.Carteira_Pessoal.Kavii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaviiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaviiApplication.class, args);
+	}
+
+}
